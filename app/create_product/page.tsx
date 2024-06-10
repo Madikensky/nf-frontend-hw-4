93d360e7862a7f702025d6b5cb979114be0dc325
Madiyar <madikensky2003@gmail.com>
2024-06-10 23:10:40 +0500

    feat: finish bronze

--- app/create_product/page.tsx
+++ app/create_product/page.tsx
@@ -1,9 +1,13 @@
 import Header from '../components/Header';
+import Footer from '../components/Footer';
+import CreateProductComponent from '../components/CreateProductComponent';
 
 export default function CreateProduct() {
   return (
-    <div>
+    <div className="flex flex-col h-screen">
       <Header />
+      <CreateProductComponent />
+      <Footer />
     </div>
   );
 }
